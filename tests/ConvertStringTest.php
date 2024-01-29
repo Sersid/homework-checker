0305d09515ef08cd972fe16d61cf9c6e8bf88d20
sers <sersoned@gmail.com>
2024-01-29 16:58:44 +0300

    Init project

--- tests/ConvertStringTest.php
+++ tests/ConvertStringTest.php
@@ -3,23 +3,49 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
+use ReflectionFunction;
 use function PHPUnit\Framework\assertSame;
 
+#[TestDox('Тесты функции convertString')]
 final class ConvertStringTest extends TestCase
 {
-    public static function dataProvider(): array
+    #[TestDox('Тест типизации аргумента $a')]
+    public function testArgumentATyping(): void
     {
-        return [
-            ['one two three', 'four', 'one two three'],
-            ['one two two', 'two', 'one two owt'],
-            ['один один два', 'один', 'один нидо два'],
-        ];
+        $reflectionFunc = new ReflectionFunction('convertString');
+
+        $a = $reflectionFunc->getParameters()[0] ?? null;
+
+        assertSame('string', $a?->getType()?->getName(), 'Аргумент не имеет тип string');
     }
 
-    #[DataProvider('dataProvider')]
-    public function test(string $a, string $b, string $expected): void
+    #[TestDox('Тест типизации аргумента $b')]
+    public function testArgumentBTyping(): void
+    {
+        $reflectionFunc = new ReflectionFunction('convertString');
+
+        $b = $reflectionFunc->getParameters()[1] ?? null;
+
+        assertSame('string', $b?->getType()?->getName(), 'Аргумент не имеет тип string');
+    }
+
+    #[TestDox('Тест возвращаемого значения')]
+    public function testReturnType(): void
+    {
+        $reflectionFunc = new ReflectionFunction('convertString');
+
+        $result = (string)$reflectionFunc->getReturnType();
+
+        assertSame('string', $result, 'Тип возвращаемого значения должен быть "string"');
+    }
+
+    #[TestWith(['two one two', 'two', 'two one owt'])]
+    #[TestWith(['один один два', 'один', 'один нидо два'])]
+    #[TestWith(['one two three', 'four', 'one two three'])]
+    public function testResult(string $a, string $b, string $expected): void
     {
         $result = convertString($a, $b);
 
