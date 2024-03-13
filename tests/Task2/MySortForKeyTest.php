78dbe3cc5b6dde756f02099fc20bfa14b99655ff
sers <sersoned@gmail.com>
2024-03-13 11:19:22 +0300

    Tests for task 2

--- tests/Task2/MySortForKeyTest.php
+++ tests/Task2/MySortForKeyTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Tests\Task2;
 
 use Exception;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
@@ -44,42 +45,48 @@ final class MySortForKeyTest extends TestCase
     }
 
     #[TestDox('Тест результата выполнения функции')]
-    #[TestWith([
-        [['a' => 2, 'b' => 1], ['a' => 1, 'b' => 3]],
-        'a',
-        [['a' => 1, 'b' => 3], ['a' => 2, 'b' => 1]]
-    ])]
-    #[TestWith([
-        [['a' => 2, 'b' => 1], ['a' => 1, 'b' => 3]],
-        'b',
-        [['a' => 2, 'b' => 1], ['a' => 1, 'b' => 3]]
-    ])]
-    #[TestWith([
-        [['a' => 2], ['a' => 1], ['a' => 3], ['a' => 10], ['a' => -12]],
-        'a',
-        [['a' => -12], ['a' => 1], ['a' => 2], ['a' => 3], ['a' => 10]]
-    ])]
-    #[TestWith([
-        [['a' => 2, 'b' => 1], ['b' => 3, 'a' => 1]],
-        'a',
-        [['b' => 3, 'a' => 1], ['a' => 2, 'b' => 1]]
-    ])]
-    #[TestWith([
-        [['a' => 2, 'b' => 1, 'c' => 456], ['b' => 3, 'a' => 1, 'c' => 123]],
-        'c',
-        [['a' => 2, 'b' => 1, 'c' => 123], ['b' => 3, 'a' => 1, 'c' => 456]]
-
-    ])]
-    #[TestWith([
-        [['a' => 1, 'c' => 123], ['c' => 456], ['b' => 3, 'c' => 123]],
-        'c',
-        [['a' => 1, 'c' => 123], ['b' => 3, 'c' => 123], ['c' => 456]]
-    ])]
+    #[DataProvider('dataProvider')]
     public function testResult(array $a, string $b, array $expected): void
     {
         $result = mySortForKey($a, $b);
 
         assertSame($expected, $result, 'Результат функции не корректный');
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            'one' => [
+                [['a' => 2, 'b' => 1], ['a' => 1, 'b' => 3]],
+                'a',
+                [['a' => 1, 'b' => 3], ['a' => 2, 'b' => 1]]
+            ],
+            'two' => [
+                [['a' => 2, 'b' => 1], ['a' => 1, 'b' => 3]],
+                'b',
+                [['a' => 2, 'b' => 1], ['a' => 1, 'b' => 3]]
+            ],
+            'three' => [
+                [['a' => 2], ['a' => 1], ['a' => 3], ['a' => 10], ['a' => -12]],
+                'a',
+                [['a' => -12], ['a' => 1], ['a' => 2], ['a' => 3], ['a' => 10]]
+            ],
+            'four' => [
+                [['a' => 2, 'b' => 1], ['b' => 3, 'a' => 1]],
+                'a',
+                [['b' => 3, 'a' => 1], ['a' => 2, 'b' => 1]]
+            ],
+            'five' => [
+                [['a' => 2, 'b' => 1, 'c' => 456], ['b' => 3, 'a' => 1, 'c' => 123]],
+                'c',
+                [['b' => 3, 'a' => 1, 'c' => 123], ['a' => 2, 'b' => 1, 'c' => 456]]
+            ],
+            'six' => [
+                [['a' => 1, 'c' => 123], ['c' => 456], ['b' => 3, 'c' => 123]],
+                'c',
+                [['a' => 1, 'c' => 123], ['b' => 3, 'c' => 123], ['c' => 456]]
+            ],
+        ];
     }
 
 
