0eb6823e48424ee4551b2adcb033de606bcc28ad
sers <sersoned@gmail.com>
2024-05-31 18:02:11 +0300

    Тесты для проверки 2 задания

---
+++ tests/Task2/DatabaseTestCase.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Task2;
+
+use PDO;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+
+abstract class DatabaseTestCase extends TestCase
+{
+    protected static PDO $pdo;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        self::connect();
+        self::createDatabase();
+        self::importSql();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+
+        self::dropDatabase();
+    }
+
+    private static function connect(): void
+    {
+        self::$pdo = new PDO(
+            'mysql:host=' . getenv('PROJECT_DB_HOST'),
+            'root',
+            'password'
+        );
+    }
+
+    private static function createDatabase(): void
+    {
+        self::$pdo->query('drop database if exists ' . getenv('PROJECT_DB_NAME'));
+        self::$pdo->query('create database ' . getenv('PROJECT_DB_NAME'));
+        self::$pdo->query('use ' . getenv('PROJECT_DB_NAME'));
+    }
+
+    private static function importSql(): void
+    {
+        $file = __DIR__ . '/../../src/2.sql';
+        $sql = file_get_contents($file);
+        self::$pdo->exec($sql);
+    }
+
+    private static function dropDatabase(): void
+    {
+        self::$pdo->query('drop database if exists ' . getenv('PROJECT_DB_NAME'));
+    }
+}
---
+++ tests/Task2/SqlFileTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Task2;
+
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\TestWith;
+use function in_array;
+use function PHPUnit\Framework\assertTrue;
+
+#[TestDox('Тесты 2.sql')]
+final class SqlFileTest extends DatabaseTestCase
+{
+    #[TestDox('БД содержит таблицу $table')]
+    #[TestWith(['a_product'])]
+    #[TestWith(['a_property'])]
+    #[TestWith(['a_price'])]
+    #[TestWith(['a_category'])]
+    public function testTableExist(string $table): void
+    {
+        $tables = $this->getTables();
+
+        assertTrue(in_array($table, $tables, true));
+    }
+
+    private function getTables(): array
+    {
+        return self::$pdo->query("SHOW TABLES")->fetchAll(\PDO::FETCH_COLUMN);
+    }
+}
