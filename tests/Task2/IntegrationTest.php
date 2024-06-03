6666b07fc55683759c1cad297ef834be54648adb
sers <sersoned@gmail.com>
2024-06-03 23:52:57 +0300

    Тесты для проверки 2 задания

--- tests/Task2/IntegrationTest.php
+++ tests/Task2/IntegrationTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Tests\Task2;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\TestDox;
 use Tests\DatabaseTestCase;
 use RuntimeException;
@@ -11,31 +12,49 @@ use function PHPUnit\Framework\assertXmlFileEqualsXmlFile;
 #[TestDox('Корректность работы импорта и экспорта данных')]
 final class IntegrationTest extends DatabaseTestCase
 {
-    public function test(): void
+    public static function dataProvider(): iterable
     {
-        $inputFile = __DIR__ . '/xml/input.xml';
-        importXml($inputFile);
-
-        $outputFile = $this->export();
-
-        assertXmlFileEqualsXmlFile($inputFile, $outputFile);
+        return [
+            'set-1' => [
+                __DIR__ . '/xml/set-1/input.xml',
+                'Бумага',
+                __DIR__ . '/xml/set-1/expected.xml',
+            ],
+            'set-2' => [
+                __DIR__ . '/xml/set-2/input.xml',
+                'Шариковые ручки',
+                __DIR__ . '/xml/set-2/expected.xml',
+            ],
+        ];
     }
 
-    public function testRepeatedImport(): void
+    #[TestDox('Базовый импорт и экспорт')]
+    #[DataProvider('dataProvider')]
+    public function test(string $inputFilename, string $categoryName, string $expectedFilename): void
     {
-        $inputFile = __DIR__ . '/xml/input.xml';
-        importXml($inputFile);
-        importXml($inputFile);
+        importXml($inputFilename);
 
-        $outputFile = $this->export();
+        $outputFile = $this->export($categoryName);
 
-        assertXmlFileEqualsXmlFile($inputFile, $outputFile);
+        assertXmlFileEqualsXmlFile($expectedFilename, $outputFile);
     }
 
-    private function export(): string
+    #[TestDox('Два импорта одного и того же файла')]
+    #[DataProvider('dataProvider')]
+    public function testRepeatedImport(string $inputFilename, string $categoryName, string $expectedFilename): void
+    {
+        importXml($inputFilename);
+        importXml($inputFilename);
+
+        $outputFile = $this->export($categoryName);
+
+        assertXmlFileEqualsXmlFile($expectedFilename, $outputFile);
+    }
+
+    private function export(string $categoryName): string
     {
         $outputFile = tempnam(sys_get_temp_dir(), 'output');
-        $categoryCode = $this->getCategoryCodeByName('Бумага');
+        $categoryCode = $this->getCategoryCodeByName($categoryName);
         exportXml($outputFile, $categoryCode);
 
         return $outputFile;
